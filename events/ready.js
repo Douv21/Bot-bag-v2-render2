4496594be34e67beb83e4db095293af189d65c6f
Douv21 <155298293+Douv21@users.noreply.github.com>
2025-07-17 22:51:47 +0200

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,14 @@
+const deployCommands = require('../utils/deployCommands');
+const dataManager = require('../utils/dataManager');
+const startKeepAlive = require('../utils/keepAlive');
 
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        console.log(`✅ Connecté en tant que ${client.user.tag}`);
+        await deployCommands(client);
+        dataManager.startAutoBackup(15);
+        startKeepAlive();
+    }
+};
