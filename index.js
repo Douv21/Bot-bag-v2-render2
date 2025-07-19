5ef90e17c922c5593b9cb7e794df1fa66e591ca8
Douv21 <155298293+Douv21@users.noreply.github.com>
2025-07-19 10:38:34 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Serveur web démarré sur le port ${PORT}`);
+});
+
+// Discord bot setup
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-require('dotenv').config();
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
 
 client.commands = new Collection();
 
-// Chargement des événements
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args, client));
+// Command loader
+const commandsPath = path.join(__dirname, 'commands');
+for (const folder of fs.readdirSync(commandsPath)) {
+  const folderPath = path.join(commandsPath, folder);
+  for (const file of fs.readdirSync(folderPath)) {
+    const filePath = path.join(folderPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
     }
+  }
+}
+
+// Event loader
+const eventsPath = path.join(__dirname, 'events');
+for (const file of fs.readdirSync(eventsPath)) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
 }
 
 client.login(process.env.TOKEN);
