91d810b22202882b25a509a72b45371a740954e1
Douv21 <155298293+Douv21@users.noreply.github.com>
2025-07-19 10:31:04 +0200

    Add files via upload

--- commands/configeconomie/config_main.js
+++ commands/configeconomie/config_main.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  run: async (interaction, client) => {
+    await interaction.reply({
+      content: '✅ Interaction "config_main" bien reçue !',
+      ephemeral: true,
+    });
+  },
+};
